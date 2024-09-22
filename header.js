e6164b70eb735bbc7ed7ae0c3d4dfdb3fd1a380a
antolinnen <privatetoli9@gmail.com>
2024-09-22 07:11:16 +0200

    first commit

---
+++ header.js
@@ -0,0 +1,7 @@
+import './header.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+
+document.querySelector('#header').innerHTML = `
+  <span class="header">Hola Header</span> 
+ `
